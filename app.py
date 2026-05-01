639a52fafa108034f8bbdbbe6fc02b11a5a36029
Keerthi S R <keerthigowda366@gmail.com>
2026-05-01 16:01:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+from groq import Groq
+
+# Initialize Groq
+client = Groq(api_key="API_KEY_HERE")
+
+st.title("🧠 AI SQL Data Analyst")
+
+file = st.file_uploader("Upload CSV")
+
+if file:
+    df = pd.read_csv(file)
+    st.write(df.head())
+
+    conn = sqlite3.connect("data.db")
+    df.to_sql("data", conn, if_exists="replace", index=False)
+
+    question = st.text_input("Ask in English")
+
+    if st.button("Get Answer"):
+        # Convert question to SQL
+        prompt = f"""
+        Convert this question into SQL query for table named data:
+        {question}
+        Only return SQL query.
+        """
+
+        response = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[{"role": "user", "content": prompt}]
+        )
+
+        sql_query = response.choices[0].message.content
+
+        sql_query = sql_query.replace("```sql", "").replace("```", "").strip()
+
+        st.write("Generated SQL:", sql_query)
+
+        result = pd.read_sql_query(sql_query, conn)
+        st.write(result)
